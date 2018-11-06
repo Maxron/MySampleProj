817541bbd3eec2cdf1fbe9ba7b8b047ff48cc7b2
Wei <zero.maxima@gmail.com>
2018-11-06 11:29:04 +0800

    [Add] Add UI sample

--- app/__main__.py
+++ app/__main__.py
@@ -1,5 +1,9 @@
 from app.domain.sayHelloUseCase import SayHello
 from app.demo import Demo
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from app.ui import mainwindow
+import sys
+
 
 def runUseCase():
     print("Hello world")
@@ -10,9 +14,20 @@ def runDemo():
     demo = Demo()
     demo.runDemo()
 
+
+def runGUI():
+    app = QApplication(sys.argv)
+    MainWindow = QMainWindow()
+    ui = mainwindow.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
+
 def main():
     runDemo()
     runUseCase()
+    runGUI()
 
 if __name__ == '__main__':
     main()
